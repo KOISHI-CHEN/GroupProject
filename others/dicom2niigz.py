5e51f333154dbe7985e5d93963b7ca032415a1f0
SUSTechkris <61013537+wyyfffff@users.noreply.github.com>
2022-01-01 21:31:45 +0800

    Update dicom2niigz.py

--- others/dicom2niigz.py
+++ others/dicom2niigz.py
@@ -6,11 +6,15 @@ import glob
 
 
 if __name__ == "__main__":
-    output_folder = "D:/GroupProjectDataSet/QIN Breast DCE-MRI/niigz/DCE-MRI-"
-    list = glob.glob("D:/GroupProjectDataSet/QIN Breast DCE-MRI/dicom/DCE-MRI*/data/57*")
+    output_folder = "D:/GroupProjectDataSet/QIN_Breast_DCE-MRI/niigz/DCE-MRI-"
+    list = glob.glob("D:/GroupProjectDataSet/QIN_Breast_DCE-MRI/dicom/DCE-MRI*/data/57*")
     i = 1
     for folder in list:
-        out_name = "D:/GroupProjectDataSet/QIN Breast DCE-MRI/niigz/DCE-MRI-{0}/data/57".format(i)
+        index = folder[56:58]
+        if folder[57:58] == '\\':
+            index = folder[56:57]
+        print(index)
+        out_name = "D:/GroupProjectDataSet/QIN_Breast_DCE-MRI/niigz/DCE-MRI-{0}/data/DCE_MRI-{1}-57".format(index, index)
         dicom2nifti.dicom_series_to_nifti(folder, out_name, reorient_nifti=True)
         print(folder)
         i = i + 1
